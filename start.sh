a9ae6fdd35919cd6da8296dea356398e174cf826
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-18 06:41:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 # npm install node-telegram-bot-api hpack https commander colors socks && pip install requests
 # Chạy bot.js trong nền
 node bot.js &
+node calva.js &
 
 # Chạy prxscan.py trong nền
 python3 prxscan.py -l list.txt &
