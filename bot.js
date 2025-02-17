5a6fe94cb0fc37c839dd7b76ca1b7f950652e64b
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-18 05:04:14 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api'), { exec } = require('child_process'), token = '7935173392:AAFYFVwBtjee7R33I64gcB3CE_-veYkU4lU', adminId = 1243471275, allowedGroupIds = new Set([-1002423723717, -1002334544605, 112233445, 556677889, 998877665]), bot = new TelegramBot(token, { polling: true }), maxSlot = 1, maxCurrent = 3, maxTimeAttacks = 120;
+const TelegramBot = require('node-telegram-bot-api'), { exec } = require('child_process'), token = '7831523452:AAH-VqWdnwRmiIaidC3U5AYdqdg04WaCzvE', adminId = 1243471275, allowedGroupIds = new Set([-1002434530321, -1002334544605, -1002365124072, 556677889, 998877665]), bot = new TelegramBot(token, { polling: true }), maxSlot = 1, maxCurrent = 3, maxTimeAttacks = 120;
 let currentProcesses = 0, queue = [], userProcesses = {}, activeAttacks = {};
 
 const restartBot = () => { console.error('🚨 Restarting bot...'); bot.stopPolling(); setTimeout(() => { bot = new TelegramBot(token, { polling: true }); initBot() }, 1000) },
